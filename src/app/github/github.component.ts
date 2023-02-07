160216e65c5f7b7fed4741a973d7a39513923991
fe-ktch <sangarefantha@gmail.com>
2023-02-07 21:49:44 +0100

    create github component

--- src/app/github/github.component.ts
+++ src/app/github/github.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-github',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubComponent implements OnInit {
 
+  clipboardCheckedVisible = false;
+  clipboardVisible = true;
+  message = new FormControl('https://github.com/fe-ktch/Angular-Testomin.git');
+
   constructor() { }
 
   ngOnInit(): void {
   }
+  copyMessage(val: string){
 
+    
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = val;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+    this.clipboardCheckedVisible = true;
+    this.clipboardVisible = false;
+
+    this.message.setValue("Copied!");
+  }
 }
