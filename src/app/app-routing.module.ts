f8c6c1fbda989bf358c142d6377180be844c37af
fe-ktch <sangarefantha@gmail.com>
2023-02-07 21:33:10 +0100

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Component, NgModule } from '@angular/core';
+import { GuardsCheckStart, RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { GithubComponent } from './github/github.component';
+import { TestominComponent } from './testomin/testomin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'about', component: AboutComponent},
+  {path: 'github', component: GuardsCheckStart},
+  {path: 'calculator', component: TestominComponent }
+];  
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
