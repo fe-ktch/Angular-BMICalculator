7bbec05f7c299aa2c17943ca5448d31fe42c5e7e
fe-ktch <sangarefantha@gmail.com>
2023-02-07 21:37:44 +0100

    create calculator component

--- src/app/testomin/testomin.component.ts
+++ src/app/testomin/testomin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-testomin',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestominComponent implements OnInit {
 
+  weight = new FormControl('');
+  height = new FormControl('');
+  bmi = new FormControl('');
+
+  bmiVisible = false;
+  message = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onKeyup(event:any) {
+    if (event.key == "Enter") {
+      this.onEnterHandler();
+    }
+  }
+
+  onEnterHandler() {
+    this.startCalculations();
+  }
+
+  startCalculations() {
+    let weight = Number(this.weight.value);
+    let height = Number(this.height.value);
+    let bmi = this.calcBMI(weight, height);
+    this.bmi.setValue(String(bmi));
+    this.bmiVisible = true;
+    this.message = true;
+  }
+
+  calcBMI(weight : number, height : number) {
+    let result = weight / Math.pow(height, 2) * 1000;
+    return result.toFixed(2);
+  }
+
+  onClickCalcButton() {
+    this.startCalculations();
+  }
 }
